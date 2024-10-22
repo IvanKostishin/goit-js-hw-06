a06c33494a0486414131eae9ca36104593c71c60
IvanKostishin <112808887+IvanKostishin@users.noreply.github.com>
2024-10-22 15:50:03 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const textInput = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
+text.style.fontSize = `${textInput.addEventListener.currentTarget.value}px`;
 textInput.addEventListener("input", (event) => {
     text.style.fontSize = `${event.currentTarget.value}px`;
 }
